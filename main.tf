362cbd5d4e485f6a08f965a79aa6c89d52e7535f
fasiahmed <syedfasiahmed83@gmail.com>
2019-09-28 23:10:08 +0200

    adjust comments

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-  region = "${var.aws_region}"
-  # shared_credentials_file = "/Users/shaik/.aws/credentials"
-  # profile                 = "fasi"
+  region     = "${var.aws_region}"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 #----storage outputs------
 
-# output "BucketName" {
-#   value = "${module.storage.bucket_out}"
-# }
-#
+output "BucketName" {
+  value = "${module.storage.bucket_out}"
+}
+
 #---Networking Outputs -----
 
 output "PublicSubnets" {
@@ -16,7 +16,6 @@ output "RdsSubnets" {
   value = "${join(", ", module.network.rds_subnets)}"
 }
 output "allSubnets" {
-  #value = "${module.network.rds_subnets}"
   value = "${split(",", join(",", module.network.rds_subnets, module.network.private_subnets, module.network.public_subnets))}"
 }
 output "rds_subnets_0" {
@@ -25,7 +24,6 @@ output "rds_subnets_0" {
 output "SubnetIPs" {
   value = "${join(", ", module.network.public_subnet_ips)}"
 }
-
 output "PublicSecurityGroup" {
   value = "${module.network.public_security_group}"
 }
@@ -36,11 +34,9 @@ output "RdsSecurityGroup" {
   value = "${module.network.rds_security_group}"
 }
 #---Compute Outputs ------
-
-# output "PublicInstanceIDs" {
-#   value = "${module.compute.server_id}"
-# }
-#
-# output "PublicInstanceIPs" {
-#   value = "${module.compute.server_ip}"
-# }
+output "PublicInstanceIDs" {
+  value = "${module.compute.server_id}"
+}
+output "PublicInstanceIPs" {
+  value = "${module.compute.server_ip}"
+}
